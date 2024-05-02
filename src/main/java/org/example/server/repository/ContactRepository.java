77eb9ec7a4bc1397339c94cffde38ad001796db0
Yevhen-Pashkulskyi <pashkulskyevhen@gmail.com>
2024-05-02 20:38:48 +0300

    modified contacr repository

--- src/main/java/org/example/server/repository/ContactRepository.java
+++ src/main/java/org/example/server/repository/ContactRepository.java
@@ -5,12 +5,12 @@ import org.example.server.entity.Contact;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DataRepository {
-    public List<Contact> fetchData(){
-        List<Contact> contacts = new ArrayList<Contact>();
-        contacts.add(new Contact(1,"Jack","555-555 5555"));
-        contacts.add(new Contact(2,"Jill","555-555 5556"));
-        contacts.add(new Contact(3,"James","555-555 5557"));
-        return contacts;
+public class ContactRepository {
+    public List<Contact> fetchContacts(){
+        List<Contact> list = new ArrayList<Contact>();
+        list.add(new Contact(1,"Jack","555-555 5555"));
+        list.add(new Contact(2,"Jill","555-555 5556"));
+        list.add(new Contact(3,"James","555-555 5557"));
+        return list;
     }
 }
